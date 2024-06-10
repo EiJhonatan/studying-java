e642b3c52e257281736e09e24ae2fdf63c347671
Jhonatan Ferreira <devjhow2003@gmail.com>
2024-06-09 22:47:35 -0300

    variavel tipo long

--- java-variaveis/src/TiposVariaveis.java
+++ java-variaveis/src/TiposVariaveis.java
@@ -1,7 +1,8 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
       // double salarioMinino = 2500.35;
-      float pi = 3.14F; // ao usa float precisa ussa o 'f' no final 
-       System.out.println(pi);
+      //float pi = 3.14F; - ao usa float precisa ussa o 'f' no final 
+      long num = 1234567891011L;// o long tambem precisa adcionar 'L' no final
+       System.out.println(num);
     }
 }

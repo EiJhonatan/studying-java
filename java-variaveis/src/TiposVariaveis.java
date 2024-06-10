345ccd623eb88a798d446886c2bf6323686a84e6
Jhonatan Ferreira <devjhow2003@gmail.com>
2024-06-09 22:40:25 -0300

    variavel tipo float

--- java-variaveis/src/TiposVariaveis.java
+++ java-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,7 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-       double salarioMinino = 2500.35;
-       System.out.println(salarioMinino);
+      // double salarioMinino = 2500.35;
+      float pi = 3.14F; // ao usa float precisa ussa o 'f' no final 
+       System.out.println(pi);
     }
 }

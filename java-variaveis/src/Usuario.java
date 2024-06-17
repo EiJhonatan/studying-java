e16c6b4576f93979cd81505fceb2f8dab0c4ddc4
Jhonatan Ferreira <devjhow2003@gmail.com>
2024-06-17 15:36:48 -0300

    requesito 1 complete

--- java-variaveis/src/Usuario.java
+++ java-variaveis/src/Usuario.java
@@ -4,8 +4,15 @@ public class Usuario {
         
         SmartTv smartTv = new SmartTv();
 
-        System.out.println("TV ligada ?"+ smartTv.ligada);
-        System.out.println("canal atual ?"+smartTv.canal);
-        System.out.println("volume Atual "+smartTv.volume);
+        System.out.println("TV ligada ? "+ smartTv.ligada);
+        System.out.println("canal atual :"+smartTv.canal);
+        System.out.println("volume Atual : "+smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status TV ligada ? "+ smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status TV ligada ? "+ smartTv.ligada);
+
     }
 }

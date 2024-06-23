580cb9b52bd195a244d87aa35764028da96d36bd
Jhonatan Ferreira <devjhow2003@gmail.com>
2024-06-23 18:08:40 -0300

    usando Switch Case

--- java-variaveis/src/SistemaMedida.java
+++ java-variaveis/src/SistemaMedida.java
@@ -1,15 +1,23 @@
 public class SistemaMedida {
     public static void main(String[] args) {
-        String sigla = "M";
+        String sigla = "P";
 
-        if(sigla == "P")
-        System.out.println("PEQUENO");
-        else if(sigla == "M")
-        System.out.println("MEDIA");
-        else if(sigla == "G")
-        System.out.println("GRANDE");
-        else 
+        switch (sigla){
+            case "P":{
+            System.out.println("PEQUENO");
+            break;
+        } 
+        case "M":{
+            System.out.println("MEDIA");
+            break;
+        } 
+        case "G":{
+            System.out.println("GRANDE");;
+            break;
+        } 
+        default:
         System.out.println("INDEFINIDO");
     }
     
+ }
 }
